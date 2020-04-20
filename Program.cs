fd063bb821dcac26092f4763058a11d918d9a2aa
Futuer Programers <63992981+FutuerPrograms@users.noreply.github.com>
2020-04-20 06:29:35 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using PointOfSell.AddForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PointOfSell
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmPowersOfUsers());
+        }
+    }
+}
